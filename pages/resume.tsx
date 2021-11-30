ef863d10b032abd0df1387b786540b56583bb783
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-11-30 23:23:58 +0700

    feat(resume): add resume page and pdf files

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -14,7 +14,16 @@ const Resume: NextPage = () => {
       </Head>
       <div className="main">
         <Header />
-        <UnderConstruction />
+        <h2>View Resume</h2>
+        <b>Select your flavor:</b>
+        <ul className="mt-0">
+          <li>
+            <a href="/files/coding_resume.pdf">Coding Resume</a>
+          </li>
+          <li>
+           <a href="/files/media_resume.pdf">Media Resume</a>
+         </li>
+        </ul>
         <Footer />
       </div>
     </>
