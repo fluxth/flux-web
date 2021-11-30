954b8ac14c8e336b64fc2670bb28b27d81a2a84f
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-12-01 02:15:42 +0700

    feat: add button to resume page

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import UnderConstruction from '../components/UnderConstruction'
+
+import styles from '../styles/Resume.module.scss'
+
+const openUrl = (url: string) => window.open(url, '_blank')
 
 const Resume: NextPage = () => {
   return (
@@ -10,19 +14,22 @@ const Resume: NextPage = () => {
       <Head>
         <title>Resume - flux.ci</title>
         <meta name="description" content="flux.ci personal website" />
+        <link rel="stylesheet" href="https://unpkg.com/98.css" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <h2>View Resume</h2>
-      <b>Select your flavor:</b>
-      <ul className="mt-0">
-        <li>
-          <a href="/files/coding_resume.pdf">Coding Resume</a>
-        </li>
-        <li>
-          <a href="/files/media_resume.pdf">Media Resume</a>
-        </li>
-      </ul>
+      <div className="text-center">
+        <h2>View Resume</h2>
+        <b>Select your flavor:</b>
+        <p className="mt-2 mb-5">
+          <button className={styles.button} onClick={() => openUrl('/files/coding_resume.pdf')}>
+            Coding Resume
+          </button>
+          <button className={styles.button} onClick={() => openUrl('/files/media_resume.pdf')}>
+            Media Resume
+          </button>
+        </p>
+      </div>
       <Footer />
     </>
   )
