d633b3cd25b7f494f5856d0709959ecb3610f512
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-12-01 23:00:45 +0700

    feat: add site analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,31 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 import '../styles/globals.scss'
 
+const GA_ID = 'G-0KXE0067PX'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Head>
         <meta property="og:image" content="/images/cover_og.png" />
         <meta name="description" content="flux.ci personal website" />
