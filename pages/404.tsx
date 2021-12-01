6fab6530212a4cac56a7e2a8fc237125ccc10307
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-12-01 14:46:19 +0700

    feat: add `Back to homepage` link in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 
 const NotFound: NextPage = () => {
   return (
@@ -9,7 +10,10 @@ const NotFound: NextPage = () => {
       </Head>
       <div className="text-center my-5">
         <h2>404 Not Found</h2>
-        <p>What you&apos;re looking for, is not here.</p>
+        <p>
+          What you&apos;re looking for, is not here.<br />
+          [<Link href="/">Back to homepage</Link>]
+        </p>
       </div>
     </>
   )
