0a1921e23cfbc05182aea4a8f9bdc9982d40f88b
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-12-01 16:35:10 +0700

    feat: detail improvements

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="mt-4 mb-3 text-muted text-center">
+  <footer className="my-4 text-muted text-center">
     &copy; {new Date().getFullYear()}, Thitat Auareesuksakul.<br />
     Inspired by homepages from early WWW era :)
   </footer>
