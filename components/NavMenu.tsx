a76ccb9a1c7ab74f947d09e36eea971f350b90d2
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-12-02 11:25:33 +0700

    feat: add analytics to NavMenu clicks

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
+
+import { trackEvent } from '../lib/ga'
+
 import { NAVMENU_ITEMS } from '../config'
 
 import styles from '../styles/NavMenu.module.scss'
@@ -8,6 +11,15 @@ export type MenuItem = {
   name: string
   image?: StaticImageData | string,
   url?: string,
+}
+
+const menuClick = (item: MenuItem) => {
+  trackEvent({
+    action: 'navmenu-click',
+    params: {
+      name: item.name
+    }
+  })
 }
 
 const NavMenu = () => (
@@ -23,9 +35,11 @@ const NavMenu = () => (
             ) : null}
             <b>
               {i.url ? (
-                <Link href={i.url}>{i.name}</Link>
+                <Link href={i.url}>
+                  <a onClick={() => menuClick(i)}>{i.name}</a>
+                </Link>
               ) : (
-                <a href="#" onClick={(e) => e.preventDefault()}>{i.name}</a>
+                <a href="#" onClick={(e) => { e.preventDefault(); menuClick(i) }}>{i.name}</a>
               )}
             </b>
           </div>
