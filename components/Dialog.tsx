54eed27f9175beb5608ce105608d8cb1fb40f0d8
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-12-05 14:52:58 +0700

    feat: add Dialog component

---
+++ components/Dialog.tsx
@@ -0,0 +1,58 @@
+import React, { useState, ReactElement } from 'react'
+import Draggable from 'react-draggable'
+
+import { useWindowDimensions } from '../lib/utils'
+
+import styles from '../styles/Dialog.module.scss'
+
+type Props = {
+  title?: string
+  shown: boolean
+  children: any
+  titleBarControls?: ReactElement
+}
+
+type State = {
+  isInactive: boolean
+}
+
+const Dialog = (props: Props) => {
+  if (!props.shown) return null
+
+  const [state, setState] = useState<State>({
+    isInactive: false,
+  })
+
+  const veilClick = () => {
+    // Flash status bar
+    setState({ isInactive: true })
+    setTimeout(() => setState({ isInactive: false }), 100)
+    setTimeout(() => setState({ isInactive: true }), 200)
+    setTimeout(() => setState({ isInactive: false }), 300)
+  }
+
+  const { width, height } = useWindowDimensions()
+
+  return (
+    <>
+      <div className={styles.veil} onClick={veilClick}></div>
+      <Draggable handle=".title-bar" positionOffset={{ x: '-50%', y: '-50%' }} disabled={width < 576}>
+        <div className={styles.dialog}>
+          <div className="window">
+            <div className={"title-bar" + (state.isInactive ? ' inactive' : '')}>
+              <div className="title-bar-text">{props.title ? props.title : 'Dialog'}</div>
+              <div className="title-bar-controls">
+                {props.titleBarControls}
+              </div>
+            </div>
+            <div className="window-body">
+              {props.children}
+            </div>
+          </div>
+        </div>
+      </Draggable>
+    </>
+  )
+}
+
+export default Dialog
