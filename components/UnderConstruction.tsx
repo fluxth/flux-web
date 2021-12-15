86c03e74110e90cd3b844ed6d81ce8b142c13226
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-12-16 00:22:04 +0700

    feat: add more cool to UnderConstruction page

--- components/UnderConstruction.tsx
+++ components/UnderConstruction.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import ConstructionBarImage from '../assets/images/construction_bar.gif'
 import UnderConstructionImage from '../assets/images/under_construction.gif'
+import UnderConstructionImage2 from '../assets/images/f_unco.png'
 
 const ConstructionBar = () => (
   <Image src={ConstructionBarImage} alt="Construction Bar" unoptimized={true} />
@@ -10,14 +11,16 @@ const UnderConstruction = () => (
   <div className="text-center">
     <ConstructionBar />
     <h1 className="mb-0">Under Construction</h1>
-    <p className="mb-4">Whoops, This page isn&apos;t available yet :(</p>
+    <p className="mb-4">
+      Whoops, This page isn&apos;t available yet :(
+      <br />
+      <i>Please check back later!</i>
+    </p>
     <ConstructionBar />
     <div className="my-4">
-      <Image
-        src={UnderConstructionImage}
-        alt="Under Construction"
-        unoptimized={true}
-      />
+      <Image src={UnderConstructionImage} alt="Under Construction" unoptimized={true} />
+      <div className="mb-3"></div>
+      <Image src={UnderConstructionImage2} alt="Under Construction" unoptimized={true} />
     </div>
   </div>
 )
